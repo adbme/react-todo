b54e60a5f1fa679ad20643cab8dd20f932412c9c
adbme <adil.bouamrioune@gmail.com>
2026-01-08 08:21:15 +0100

    fix(ui): remove accidental 'max-h-lg:bg-red-200' from header class

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import profilePicture from '../../assets/profile-picture.svg';
 
 const Header = ({ activeIndex }: any) => {
     return (
-        <header className={`z-40 fixed w-screen top-0 px-10 transition-all duration-700 ease-in-out max-h-lg:bg-red-200  
+        <header className={`z-40 fixed w-screen top-0 px-10 transition-all duration-700 ease-in-out  
             ${activeIndex === 1 ? 'py-20 bg-white border-b border-b-gray-200' : 'py-10 border-b border-b-white/0'}`}
         >
             <div className='flex justify-between w-full max-w-7xl mx-auto '>
