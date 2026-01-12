32745064e8153593578b268d80bbf14f734cc6a4
adbme <adil.bouamrioune@gmail.com>
2026-01-12 12:53:15 +0100

    feat: add react-error-boundary for error handling

--- src/views/TodoListView.tsx
+++ src/views/TodoListView.tsx
@@ -6,13 +6,11 @@ import OptionButton from '../components/ui/OptionButton';
 import { use, Suspense } from 'react';
 import Spinner from '../components/ui/Spinner';
 import { toast } from 'react-toastify';
+import { ErrorBoundary } from 'react-error-boundary';
 
-
-
-const TodoList = () => {
-  const todos = use(fetchTodos);
-  if (todos.length === 0) {
-    return (
+const TodoError = () => {
+  return (
+    <>
       <div className="animate-entry-up col-span-full flex flex-col items-center justify-center p-12 border-2 border-dashed border-gray-300 rounded-2xl bg-white/30 backdrop-blur-sm transition-all animate-in fade-in zoom-in duration-500">
         <div className="bg-black p-4 rounded-full mb-4">
           <svg className="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -22,9 +20,12 @@ const TodoList = () => {
         <p className="text-xl font-semibold text-gray-600">No tasks to complete</p>
         <p className="text-gray-400 mt-1">Everything is up to date.</p>
       </div>
-    );
-  }
+    </>
+  )
+}
 
+const TodoList = () => {
+  const todos = use(fetchTodos);
   return (
     <>
       {todos.map((todo, index) => (
@@ -45,17 +46,16 @@ const TodoList = () => {
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 const fetchTodos = fetch("https://api.todos.in.jt-lab.ch:443/todos")
   .then(async (res) => {
-    await delay(5000); 
+    await delay(5000);
 
-    if (!res.ok) throw new Error('API Error'); 
-    
+    if (!res.ok) throw new Error('API Error');
+
     return res.json() as Promise<TodoItemProps[]>;
   })
   .catch((err) => {
-    toast.error("Failed to fetch todos");
-    return [] as TodoItemProps[]; 
+    throw new Error("Failed to fetch todos, check network")
   });
-  
+
 
 
 const TodoListView = () => {
@@ -78,11 +78,11 @@ const TodoListView = () => {
         </div>
 
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-4">
-
-          <Suspense fallback={<Spinner variant="button" text="Loading todos" />}>
-            <TodoList />
-          </Suspense>
-
+          <ErrorBoundary fallback={<TodoError />} onError={(error) => {toast.error(error.message);}}>
+            <Suspense fallback={<Spinner variant="button" text="Loading todos" />}>
+              <TodoList />
+            </Suspense>
+          </ErrorBoundary>
         </div>
       </div>
     </div>
