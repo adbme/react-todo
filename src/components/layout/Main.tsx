8f76d3fbb1465c749aad144379b8bcd1efe83ed7
adbme <adil.bouamrioune@gmail.com>
2026-01-15 16:19:19 +0100

    feat: add updateTodo function and pass it to TodoListView for updating todos

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -26,6 +26,15 @@ const Main = memo(({ setSwiper, setActiveIndex, swiper }: MainProps) => {
     setTodos((prevTodos) => prevTodos.filter(todo => todo.id !== id));
   }, []);
 
+  const updateTodo = (id: number, updatedTodo: Partial<FullTodo>) => {
+    setTodos((prevTodos) => prevTodos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, ...updatedTodo };
+      }
+      return todo;
+    }));
+  }
+
   return (
     <main className="w-screen">
       <Swiper
@@ -34,7 +43,7 @@ const Main = memo(({ setSwiper, setActiveIndex, swiper }: MainProps) => {
         onSlideChange={(s) => setActiveIndex(s.activeIndex)}
       >
         <SwiperSlide>
-          <TodoListView todos={todos} onTodoDeleted={removeTodo} />
+          <TodoListView todos={todos} onTodoDeleted={removeTodo} onTodoUpdated={updateTodo} />
         </SwiperSlide>
 
         <SwiperSlide>
