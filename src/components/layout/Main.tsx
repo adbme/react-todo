39f37ad2d1c662e8b1c6af65799b63419c2f4817
adbme <adil.bouamrioune@gmail.com>
2026-01-19 16:15:50 +0100

    feat: implement sorting functionality for todos based on selected option

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,4 +1,4 @@
-import { memo, use, useCallback, useState } from 'react';
+import { memo, use, useCallback, useEffect, useState } from 'react';
 import type { Swiper as SwiperType } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import LandingView from '../../views/LandingView';
@@ -12,11 +12,49 @@ type MainProps = {
   swiper: SwiperType | null
 }
 
+interface Option {
+  label: string,
+  value: string,
+  sortFunction?: (todos: FullTodo[]) => FullTodo[]
+}
+
+const sortTodoByName = (todos: FullTodo[]) => {
+
+  const sortedTodos = [...todos].sort((a, b) => {
+    return a.title.localeCompare(b.title);
+  });
+
+  return sortedTodos;
+}
+
+let options: Option[];
+
+const sortTodoByDueDate = (todos: FullTodo[]) => {
+  const sortedTodos = [...todos].sort((a, b) => {
+    return new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime();
+  });
+
+  return sortedTodos;
+}
+
+const sortTodoByDone = (todos: FullTodo[], doneStatus: boolean) => {
+  const sortedTodos = todos.filter(todo => todo.done === doneStatus);
+  return sortedTodos;
+}
+
+options = [
+  { label: 'Done', value: 'done', sortFunction: (todos) => sortTodoByDone(todos, true) },
+  { label: 'Undone', value: 'undone', sortFunction: (todos) => sortTodoByDone(todos, false) },
+  { label: 'Sort by name (a-z)', value: 'name', sortFunction: sortTodoByName },
+  { label: 'Sort by due date (soonest first)', value: 'dueDate', sortFunction: sortTodoByDueDate },
+];
+
 const todoPromise = fetchTodos();
 
 const Main = memo(({ setSwiper, setActiveIndex, swiper }: MainProps) => {
   const initalTodos = use(todoPromise);
   const [todos, setTodos] = useState(initalTodos);
+  const [sortOption, setSortOption] = useState<string>('');
 
   const addTodo = (newTodo: FullTodo) => {
     setTodos((prevTodos) => [...prevTodos, newTodo]);
@@ -26,7 +64,7 @@ const Main = memo(({ setSwiper, setActiveIndex, swiper }: MainProps) => {
     setTodos((prevTodos) => prevTodos.filter(todo => todo.id !== id));
   }, []);
 
-    const updateTodo = useCallback((id: number, updatedTodo: Partial<FullTodo>) => {
+  const updateTodo = useCallback((id: number, updatedTodo: Partial<FullTodo>) => {
     setTodos((prevTodos) => prevTodos.map(todo => {
       if (todo.id === id) {
         return { ...todo, ...updatedTodo };
@@ -34,6 +72,30 @@ const Main = memo(({ setSwiper, setActiveIndex, swiper }: MainProps) => {
       return todo;
     }));
   }, []);
+
+  const sortTodos = (option: string) => {
+    setSortOption(option);
+
+    console.log("Sorting todos by:", option);
+
+    // const selectedOption = options.find(opt => opt.value === option);
+    // if (selectedOption && selectedOption.sortFunction) {
+    //   setTodos((prevTodos) => {
+    //     const todosCopy = [...prevTodos];
+    //     todosCopy.sort(selectedOption.sortFunction);
+    //     return todosCopy;
+    //   });
+    // }
+  }
+
+  let todosToDisplay = todos;
+  const selectedOption = options.find(opt => opt.value === sortOption);
+    if (selectedOption && selectedOption.sortFunction) {
+      console.log(todos)
+      todosToDisplay = selectedOption.sortFunction(todos);
+
+      console.log("Todos sorted:", todosToDisplay);
+    }
 
   return (
     <main className="w-screen">
@@ -43,7 +105,7 @@ const Main = memo(({ setSwiper, setActiveIndex, swiper }: MainProps) => {
         onSlideChange={(s) => setActiveIndex(s.activeIndex)}
       >
         <SwiperSlide>
-          <TodoListView todos={todos} onTodoDeleted={removeTodo} onTodoUpdated={updateTodo} />
+          <TodoListView todos={todosToDisplay} onTodoDeleted={removeTodo} onTodoUpdated={updateTodo} options={options} sortOption={sortOption} onTodoSorted={sortTodos} />
         </SwiperSlide>
 
         <SwiperSlide>
