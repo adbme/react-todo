444862623412ed785bc1fb089748e49c35b53bf4
adbme <adil.bouamrioune@gmail.com>
2026-01-21 09:15:03 +0100

    feat(store): add centralized zustand store for todo management

---
+++ src/store.ts
@@ -0,0 +1,43 @@
+import { create } from 'zustand';
+import type { Swiper as SwiperType } from 'swiper';
+import type { FullTodo } from './components/Todo/TodoItem';
+
+interface TodoState {
+  todos: FullTodo[];
+  sortOption: string;
+
+  swiper: SwiperType | null;
+  activeIndex: number;
+  
+  setTodos: (todos: FullTodo[]) => void;
+  addTodo: (todo: FullTodo) => void;
+  removeTodo: (id: number) => void;
+  updateTodo: (id: number, updatedFields: Partial<FullTodo>) => void;
+  setSortOption: (option: string) => void;
+  
+  setSwiper: (swiper: SwiperType | null) => void;
+  setActiveIndex: (index: number) => void;
+  goToSlide: (index: number) => void;
+}
+
+export const useTodoStore = create<TodoState>((set, get) => ({
+  todos: [],
+  sortOption: '',
+  swiper: null,
+  activeIndex: 0,
+  
+  setTodos: (todos) => set({ todos }),
+  addTodo: (newTodo) => set((state) => ({ todos: [...state.todos, newTodo] })),
+  removeTodo: (id) => set((state) => ({ todos: state.todos.filter(t => t.id !== id) })),
+  updateTodo: (id, updatedFields) => set((state) => ({
+    todos: state.todos.map(t => t.id === id ? { ...t, ...updatedFields } : t)
+  })),
+  setSortOption: (option) => set({ sortOption: option }),
+
+  setSwiper: (swiper) => set({ swiper }),
+  setActiveIndex: (activeIndex) => set({ activeIndex }),
+  goToSlide: (index) => {
+    const swiper = get().swiper;
+    if (swiper) swiper.slideTo(index);
+  }
+}));
